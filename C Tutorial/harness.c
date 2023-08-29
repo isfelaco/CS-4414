3eba3d2b9e7397f0997db92e080ba711836dbc62
isfelaco <isfelaco@gmail.com>
2023-08-29 13:14:46 -0400

    exiting/error handling

--- C Tutorial/harness.c
+++ C Tutorial/harness.c
@@ -15,19 +15,19 @@ int main(int argc, char* argv[]) {
     FILE *file = fopen(argv[1], "r");
 
     if ( file == 0 ) {
-        printf( "Could not open file\n" );
+        perror("Could not open file");
+        exit(-1);
     }
     else {
         if (argc > 2) {
             char line[1024];
             if (strcmp(argv[2], "echo") == 0) { 
-                // display the input, one word 
-                // (continguous string of non-whitespace characters) 
-                // per line, and exit
+                // display the input, one word (continguous string of non-whitespace characters) per line, and exit
                 while  (fgets(line, sizeof(line), file) != NULL) {
                     printf("%s", line);
                 }
-                // exit
+                fclose(file);
+                exit(-1);
             }
             else if (strcmp(argv[2], "tail") == 0) {
                 // after input has been added to list, print contents
@@ -36,7 +36,8 @@ int main(int argc, char* argv[]) {
                     list_insert_tail(&l, line_copy);
                 }
                 list_visit_items(&l, list_print_item);
-                // exit
+                fclose(file);
+                exit(-1);
             }
             else if (strcmp(argv[2], "tail-remove") == 0){
                 while  (fgets(line, sizeof(line), file) != NULL) {
@@ -47,10 +48,14 @@ int main(int argc, char* argv[]) {
                 list_remove_head(&l);
                 list_remove_head(&l);
                 list_visit_items(&l, list_print_item);
-                // exit
+                fclose(file);
+                exit(-1);
             }
+        } else {
+            printf("Please provide a second parameter, either \'echo\', \'tail\', or \'tail-remove\'.\n");
             fclose(file);
-        }   
+            exit(-1);
+        }  
     }
     return 0;
 }
