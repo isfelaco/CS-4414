7f5e945b47fd078204d9580217a75db07ecfa731
isfelaco <isfelaco@gmail.com>
2023-09-12 18:15:33 -0400

    Shell pt2: Comments & fixed parsing

--- Shell Part 1/shell/main.cc
+++ Shell Part 1/shell/main.cc
@@ -26,7 +26,8 @@ void parse_and_run_command(const string &command) {
     /* PARSE THE INPUT */
     /* Parse the commnd line, separating tokens by whitespace */
     /* source: https://www.geeksforgeeks.org/split-a-sentence-into-words-in-cpp/ */
-    vector<string> tokens;
+    vector<vector<string>> commands;
+    vector<string> tokens;;
     string tempWord;
     for (char c : command) {
         // Check if the character is a whitespace character
@@ -36,6 +37,13 @@ void parse_and_run_command(const string &command) {
                 tokens.push_back(tempWord);
                 tempWord.clear();
             }
+        }
+        else if (c == '|') {
+            if (!tempWord.empty()) {
+                tokens.push_back(tempWord);
+            }
+            commands.push_back(tokens);
+            tokens.clear();
         } else {
             // If the character is not a whitespace character, add it to the temporary string
             tempWord.push_back(c);
@@ -43,6 +51,9 @@ void parse_and_run_command(const string &command) {
     }
     if (!tempWord.empty()) {
         tokens.push_back(tempWord);
+    }
+    if (!tokens.empty()) {
+        commands.push_back(tokens);
     }
     /* end source */
 
@@ -54,103 +65,122 @@ void parse_and_run_command(const string &command) {
     string cmd_str;
     vector<string> cmd_args;
 
-    /* Find any exits or redirects in the tokens */
-    for (unsigned int i = 0; i < tokens.size(); ++i) {
-        string token = tokens[i];
-        if (token == "exit") {
-            exit(0);
-        }
-        else if (token == "<") {
-            // next token is the input file
-            if (i + 1 < tokens.size()) {
-                redirect_input = true;
-                if (tokens[i+1] == "<" || tokens[i+1] == ">") {
+    for (vector<string> c : commands) {
+        /* Find any exits or redirects in the tokens */
+        for (unsigned int i = 0; i < c.size(); ++i) {
+            string token = c[i];
+            if (token == "exit") {
+                exit(0);
+            }
+            else if (token == "<") {
+                // next token is the input file
+                if (i + 1 < c.size()) {
+                    redirect_input = true;
+                    if (c[i+1] == "<" || c[i+1] == ">") {
+                        cerr << "invalid command" << endl;
+                    }
+                    input_file = c[i + 1].c_str();
+                    i++;
+                } else {
                     cerr << "invalid command" << endl;
+                    return;
                 }
-                input_file = tokens[i + 1].c_str();
-                i++;
-            } else {
-                cerr << "invalid command" << endl;
-                return;
             }
-        }
-        else if (token == ">") {
-            // next token is the output file
-            if (i + 1 < tokens.size()) {
-                redirect_output = true;
-                if (tokens[i+1] == "<" || tokens[i+1] == ">") {
+            else if (token == ">") {
+                // next token is the output file
+                if (i + 1 < c.size()) {
+                    redirect_output = true;
+                    if (c[i+1] == "<" || c[i+1] == ">") {
+                        cerr << "invalid command" << endl;
+                    }
+                    output_file = c[i + 1].c_str();
+                    i++;
+                } else {
                     cerr << "invalid command" << endl;
+                    return;
                 }
-                output_file = tokens[i + 1].c_str();
-                i++;
-            } else {
-                cerr << "invalid command" << endl;
-                return;
+            }
+            else if (cmd_str.empty()) {
+                // token is the command
+                cmd_str = token;
+            }
+            else {
+                // token is an argument to the command
+                cmd_args.push_back(token);
             }
         }
-           else if (cmd_str.empty()) {
-            // token is the command
-            cmd_str = token;
-        }
-        else {
-            // token is an argument to the command
-            cmd_args.push_back(token);
-        }
-    }
+        /* EXECUTE THE COMMAND */
 
-    /* EXECUTE THE COMMAND */
-    /* Fork */
-    pid_t pid = fork();
+        /*
+        if (i >= 0 && i < totalcmd-1) // there are multiple commands
+            pipe(int pipefd[2]) // pipe array is used to return two file descriptors referring to the ends of the pipe
+        Pid = Fork()
+        if (pid==0) // in child process
+            Do redirection if there is any redirection in this command
+        if (i > 0) // if command not the first command
+            close(stdin)
+            dup(previous_pipe_out) // read from the write end of previous pipe
+        if (i < totalcmds - 1) // not last command ?
+            close(stdout) // close out file
+            dup(pipe_out) // open the pipe write end to write
+        close the pipes that are not needed
 
-    if (pid == -1) { // fork error
-        cerr << "Fork failed" << endl;
-        cout << "> " << endl;
-        exit(1);
-    } else if (pid == 0) { // child proces
-        /* Redirect first */
-        if (redirect_input) {
-            int fd =  open(input_file, O_RDONLY);
-            if (fd == -1) {
-                perror("open"); // need to fix i think
-                exit(1);
-            }
-            dup2(fd, STDIN_FILENO); // close stdin, copy file descriptor fd into standard input
-            close(fd); // close file descriptor       
-        }
-        if (redirect_output) {
-            int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (fd == -1) {
-                perror("open"); // need to fix i think
-                exit(1);
-            }
-            dup2(fd, STDOUT_FILENO); // close stdout, copy file descriptor fd into standard output
-            close(fd); // close file descriptor
-        }
+        if it is the last command, we want to write to the termina/redirection file
+        */
 
-        /* Convert string vector to char array to pass to execvp */
-        char *cmd = const_cast<char *>(cmd_str.c_str());
-        char **argv = new char *[cmd_args.size() + 2]; // +2 for command and nullptr
-        argv[0] = cmd;
-        for (size_t i = 0; i < cmd_args.size(); ++i) {
-            argv[i + 1] = const_cast<char *>(cmd_args[i].c_str());
-        }
-        argv[cmd_args.size() + 1] = nullptr;
+        /* Fork */
+        pid_t pid = fork();
 
-
-        /* Run the command */
-        int exec_status = execv(cmd, argv);
-        if (exec_status == -1) {
-            cerr << "invalid command / Command not found" << endl;
+        if (pid == -1) { // fork error
+            cerr << "Fork failed" << endl;
+            cout << "> " << endl;
             exit(1);
+        } else if (pid == 0) { // child process
+            /* Redirect first */
+            if (redirect_input) {
+                int fd =  open(input_file, O_RDONLY);
+                if (fd == -1) {
+                    perror("open"); // need to fix i think
+                    exit(1);
+                }
+                dup2(fd, STDIN_FILENO); // close stdin, copy file descriptor fd into standard input
+                close(fd); // close file descriptor       
+            }
+            if (redirect_output) {
+                int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if (fd == -1) {
+                    perror("open"); // need to fix i think
+                    exit(1);
+                }
+                dup2(fd, STDOUT_FILENO); // close stdout, copy file descriptor fd into standard output
+                close(fd); // close file descriptor
+            }
+
+            /* Convert string vector to char array to pass to execvp */
+            char *cmd = const_cast<char *>(cmd_str.c_str());
+            char **argv = new char *[cmd_args.size() + 2]; // +2 for command and nullptr
+            argv[0] = cmd;
+            for (size_t i = 0; i < cmd_args.size(); ++i) {
+                argv[i + 1] = const_cast<char *>(cmd_args[i].c_str());
+            }
+            argv[cmd_args.size() + 1] = nullptr;
+
+
+            /* Run the command */
+            int exec_status = execv(cmd, argv);
+            if (exec_status == -1) {
+                cerr << "invalid command / Command not found" << endl;
+                exit(1);
+            }
+        } else { // parent process
+            int status;
+            waitpid(pid, &status, 0);
+            if (WIFEXITED(status)) {
+                /* Print the exit status of the child process */
+                cout << cmd_str << " exit status: " << WEXITSTATUS(status) << endl;
+            }
         }
-    } else { // parent process
-        int status;
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status)) {
-            /* Print the exit status of the child process */
-            cout << cmd_str << " exit status: " << WEXITSTATUS(status) << endl;
-        }
-    }
+    }   
 }
 
 int main(void) {
