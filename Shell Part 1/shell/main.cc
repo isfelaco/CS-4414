6aedf01b82367b84f7a0c525faad95e16892e951
isfelaco <isfelaco@gmail.com>
2023-09-14 17:56:07 -0400

    Comments/cleanup

--- Shell Part 1/shell/main.cc
+++ Shell Part 1/shell/main.cc
@@ -11,18 +11,6 @@
 using namespace std;
 
 void parse_and_run_command(const string &command) {
-    /*
-    REQUIREMENTS:
-        run simple commands (e.g. /bin/cat foo.txt bar.txt)
-        input redirection (e.g. /usr/bin/gcc -E < somefile.txt)
-        output redirection (e.g. /usr/bin/gcc -E > somefile.txt)
-        pipelines of multiple commands (e.g. /bin/cat foo.txt | /bin/grep bar | /bin/grep baz > output.txt)
-        builtin command exit
-        outputs the exit status of each command
-        prints out error messages to stderr (e.g. via std:cerr)
-        does not involve the system's shell
-    */
-   
     /* PARSE THE INPUT */
     /* Parse the commnd line, separating tokens by whitespace */
     /* source: https://www.geeksforgeeks.org/split-a-sentence-into-words-in-cpp/ */
@@ -39,7 +27,7 @@ void parse_and_run_command(const string &command) {
                     tokens.push_back(tempWord);
                     tempWord.clear();
                 }
-                commands.push_back(tokens);
+                commands.push_back(tokens); // end of command
                 tokens.clear();
                 i += 2;
             }
@@ -48,26 +36,20 @@ void parse_and_run_command(const string &command) {
                 cerr << "invalid command" << endl;
                 exit(1);
             }
-            // If the temporary string is not empty, add it to the list of words
             else if (!tempWord.empty()) {
                 tokens.push_back(tempWord);
                 tempWord.clear();
-            }
-            
+            }  
         }
-        else {
-            // If the character is not a whitespace character, add it to the temporary string
-            tempWord.push_back(c);
-        }
+        else { tempWord.push_back(c); }
     }
-    if (!tempWord.empty()) {
-        tokens.push_back(tempWord);
-    }
-    if (!tokens.empty()) {
-        commands.push_back(tokens);
-    }
+    // end of input
+    if (!tempWord.empty()) { tokens.push_back(tempWord); }
+    if (!tokens.empty()) { commands.push_back(tokens); }
     /* end source */
 
+    /* EXECUTE THE COMMAND */
+    vector<int> statuses;
     int prev_pipefd[2];
 
     for (unsigned int j = 0; j < commands.size(); j++) {
@@ -82,16 +64,12 @@ void parse_and_run_command(const string &command) {
         /* Find any exits or redirects in the tokens */
         for (unsigned int i = 0; i < c.size(); ++i) {
             string token = c[i];
-            if (token == "exit") {
-                exit(0);
-            }
+            if (token == "exit") { exit(0); }
             else if (token == "<") {
                 // next token is the input file
                 if (i + 1 < c.size()) {
                     redirect_input = true;
-                    if (c[i+1] == "<" || c[i+1] == ">") {
-                        cerr << "invalid command" << endl;
-                    }
+                    if (c[i+1] == "<" || c[i+1] == ">") { cerr << "invalid command" << endl; }
                     input_file = c[i + 1].c_str();
                     i++;
                 } else {
@@ -103,9 +81,7 @@ void parse_and_run_command(const string &command) {
                 // next token is the output file
                 if (i + 1 < c.size()) {
                     redirect_output = true;
-                    if (c[i+1] == "<" || c[i+1] == ">") {
-                        cerr << "invalid command" << endl;
-                    }
+                    if (c[i+1] == "<" || c[i+1] == ">") { cerr << "invalid command" << endl; }
                     output_file = c[i + 1].c_str();
                     i++;
                 } else {
@@ -113,16 +89,9 @@ void parse_and_run_command(const string &command) {
                     return;
                 }
             }
-            else if (cmd_str.empty()) {
-                // token is the command
-                cmd_str = token;
-            }
-            else {
-                // token is an argument to the command
-                cmd_args.push_back(token);
-            }
+            else if (cmd_str.empty()) { cmd_str = token; } // first non-redirection token is the command
+            else { cmd_args.push_back(token); }  // token is an argument to the command
         }
-        /* EXECUTE THE COMMAND */
         /* Pipe */
         int pipefd[2];
         if (j >= 0 && j < commands.size() - 1) { // not the last command
@@ -136,29 +105,38 @@ void parse_and_run_command(const string &command) {
         pid_t pid = fork();
 
         if (pid == -1) { // fork error
-            /* TODO: this shouldn't exit, it should return to next input */
             cerr << "Fork failed" << endl;
-            cout << "> " << endl;
-            exit(1);
         } else if (pid == 0) { // child process
             /* Redirect first */
             if (redirect_input) {
                 int fd =  open(input_file, O_RDONLY);
                 if (fd == -1) {
-                    perror("open"); // need to fix i think
+                    perror("open");
                     exit(1);
                 }
                 dup2(fd, STDIN_FILENO); // close stdin, copy file descriptor fd into standard input
-                close(fd); // close file descriptor       
+                close(fd); // close file descriptor (already duplicated)
             }
             if (redirect_output) {
                 int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
                 if (fd == -1) {
-                    perror("open"); // need to fix i think
+                    perror("open");
                     exit(1);
                 }
                 dup2(fd, STDOUT_FILENO); // close stdout, copy file descriptor fd into standard output
-                close(fd); // close file descriptor
+                close(fd); // close file descriptor (already duplicated)
+            }
+
+            /* Connect pipes from previous command */
+            if (j > 0) { // not the first command
+                dup2(prev_pipefd[0], STDIN_FILENO); // read from the read end of previous pipe
+                close(prev_pipefd[0]); // close original read end of the previous pipe (already duplicated)
+                close(prev_pipefd[1]); // close original write end of the previous pipe (not needed)
+            }
+            if (j < commands.size() - 1) { // not last command   
+                close(pipefd[0]); // close read end of current pipe
+                dup2(pipefd[1], STDOUT_FILENO); // open the pipe write end to write
+                close(pipefd[1]); // close the write end of the pipe (already duplicated)
             }
 
             /* Convert string vector to char array to pass to execvp */
@@ -169,17 +147,6 @@ void parse_and_run_command(const string &command) {
                 argv[i + 1] = const_cast<char *>(cmd_args[i].c_str());
             }
             argv[cmd_args.size() + 1] = nullptr;
-
-            if (j > 0) { // not the first command
-                dup2(prev_pipefd[0], STDIN_FILENO); // read from the write end of previous pipe
-                close(prev_pipefd[0]); // close original read end of the pipe
-                close(prev_pipefd[1]); // close write end of pipe in the current process
-            }
-            if (j < commands.size() - 1) { // not last command   
-                close(pipefd[0]); // close read end of pipe in current process
-                dup2(pipefd[1], STDOUT_FILENO); // open the pipe write end to write
-                close(pipefd[1]);
-            }
 
             /* Run the command */
             if (access(cmd, X_OK) == 0) { // Check if the command is executable
@@ -200,6 +167,7 @@ void parse_and_run_command(const string &command) {
                 close(prev_pipefd[1]);
             }
 
+            // set the previous pipe to be the current pipe
             prev_pipefd[0] = pipefd[0];
             prev_pipefd[1] = pipefd[1];
 
