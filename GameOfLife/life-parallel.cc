8804ec0fd8073486aeb42ec5c9ee0d4e569957fb
isfelaco <isfelaco@gmail.com>
2023-10-09 19:07:53 -0400

    Working threading

--- GameOfLife/life-parallel.cc
+++ GameOfLife/life-parallel.cc
@@ -26,38 +26,40 @@ void* func(void* arg) {
     int thread_id = info->Thread_id;
     int num_threads = info->num_threads;
     int steps = info->steps;
-    LifeBoard* state = &(info->state);
-    LifeBoard* next_state = &(info->next_state);
+    LifeBoard state = info->state;
+    LifeBoard next_state = info->next_state;
     pthread_barrier_t* barrier = info->barrier;
 
     // Wait behind barrier until other threads finish their job for this step
     pthread_barrier_wait(barrier);
 
+    int height = state.height();
+
     // assign a chunk of state to a thread based on a predefined thread id
-    int rows_per_thread = state->height() / num_threads;
+    int rows_per_thread = height / num_threads;
     int start_row = thread_id * rows_per_thread;
-    int end_row = (thread_id == num_threads - 1) ? state->height() : start_row + rows_per_thread;
+    int end_row = (thread_id == num_threads - 1) ? height : start_row + rows_per_thread;
 
     // Count live neighbours and Update next state is the same as single thread
-    for (int generation = 0; generation < steps; generation++) {
+    for (int step = 0; step < steps; ++step) {
         // Process the assigned portion of the board
-        for (int x = start_row; x < end_row; x++) {
-            for (int y = 1; y < state->width() -1; y++) {
+        for (int y = 1; y < height - 1; ++y) {
+            for (int x = start_row; x < end_row; ++x) {
                 int live_in_window = 0;
                 /* For each cell, examine a 3x3 "window" of cells around it,
                  * and count the number of live (true) cells in the window. */
                 for (int y_offset = -1; y_offset <= 1; ++y_offset) {
                     for (int x_offset = -1; x_offset <= 1; ++x_offset) {
-                        if (state->at(x + x_offset, y + y_offset)) {
+                        if (state.at(x + x_offset, y + y_offset)) {
                             ++live_in_window;
                         }
                     }
                 }
                 /* Cells with 3 live neighbors remain or become live.
                    Live cells with 2 live neighbors remain live. */
-                next_state->at(x, y) = (
+               next_state.at(x, y) = (
                     live_in_window == 3 /* dead cell with 3 neighbors or live cell with 2 */ ||
-                    (live_in_window == 4 && state->at(x, y)) /* live cell with 3 neighbors */
+                    (live_in_window == 4 && state.at(x, y)) /* live cell with 3 neighbors */
                 );
             }
         }
@@ -111,5 +113,8 @@ void simulate_life_parallel(int threads, LifeBoard &state, int steps) {
         }
     }
 
-    pthread_barrier_destroy(&barrier);
+    int destroy_result = pthread_barrier_destroy(&barrier);
+    if (destroy_result != 0) {
+        perror("pthread_barrier_destroy");
+    }
 }
